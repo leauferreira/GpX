9baa54d2faa8e40263a2b141d2929528c23d20e3
leauferreira <leauferreira@hotmail.com>
2020-09-08 16:33:18 -0300

    bug in feature distribution names - dict with get.

--- gp_explainer/gpx.py
+++ gp_explainer/gpx.py
@@ -136,8 +136,7 @@ class Gpx:
                                          'feature_names': self.feature_names}
         else:
             self._gp_hyper_parameters = gp_hyper_parameters
-            self.feature_names = gp_hyper_parameters['feature_names']
-
+            self.feature_names = gp_hyper_parameters.get('feature_names')
 
     @property
     def feature_names(self):
